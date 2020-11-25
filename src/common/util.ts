2079f389472d5a4f236b61ce68854572debcb787
Karibash <y.karibayashi@gmail.com>
2020-11-25 22:38:22 +0900

    Wording fixes

--- src/common/util.ts
+++ src/common/util.ts
@@ -1,19 +1,19 @@
-export const findBaseFontSize = (): number|null => {
+export const findRootFontSize = (): number|null => {
   if (typeof document !== 'undefined') {
     return parseFloat(getComputedStyle(document.documentElement).fontSize);
   }
   return null;
 };
 
-export const tryFindBaseFontSize = (defaultSize?: number): number => {
-  const baseSize = findBaseFontSize();
-  if (typeof baseSize === 'number') {
-    return baseSize;
+export const tryFindRootFontSize = (defaultSize?: number): number => {
+  const rootFontSize = findRootFontSize();
+  if (typeof rootFontSize === 'number') {
+    return rootFontSize;
   }
 
   if (typeof defaultSize === 'number') {
     return defaultSize;
   }
 
-  throw new Error('The font size of the root element could not be obtained. Run in a browser or specify an initial size.');
+  throw new Error('The font size of the root element could not be obtained. Run in a browser or specify an default size.');
 };
